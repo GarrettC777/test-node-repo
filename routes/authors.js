5f75d8e1971d4c53c433f4863a7cadd04034d255
garrettc777 <garrett.church.820@my.csun.edu>
2021-05-03 00:03:12 -0700

    added more.

---
+++ routes/authors.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All authors route
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+// New authors route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+})
+
+// Create author route
+router.post('/', (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    author.save((err, newAuthor) => {
+        if (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error Creating Author'
+            })
+        } else {
+            //res.redirect('authors/${newAuthor.id')
+            res.redirect('authors')
+        }
+    })
+})
+
+module.exports = router
